edea4c5fa5b5e0f87bcf0b70f74866c0017f4610
Roman Bulakh <bulah.roman@gmail.com>
2015-05-23 22:17:30 +0300

    Замена запускалки на rofi

--- rc.lua
+++ rc.lua
@@ -398,7 +398,8 @@ globalkeys = awful.util.table.join(
   awful.key({ }, "XF86AudioMute",         APW.ToggleMute),
 
   -- Prompt
-  awful.key({ modkey },            "r",     function () mypromptbox[mouse.screen]:run() end),
+  --awful.key({ modkey },            "r",     function () mypromptbox[mouse.screen]:run() end),
+  awful.key({ modkey },            "r",     function () awful.util.spawn("rofi -show run -font 'snap 10' -fg '#505050' -bg '#000000' -hlfg '#ffb964' -hlbg '#000000' -o 85")  end),
 
   awful.key({ modkey },            "x",
     function ()
